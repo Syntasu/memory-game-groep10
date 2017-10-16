7e8db38583101f730469d7196378ac97be7e68f5
Alex R.L <alexrlopez03@gmail.com>
2017-10-16 16:36:38 +0200

    Test

--- MemoryGameProject/Alexander.cs
+++ MemoryGameProject/Alexander.cs
@@ -30,6 +30,8 @@ namespace MemoryGameProject
             List<string> Names = new List<string>();
             Names.Add(tbUsername.Text);
 
+            lbPlayers.DataSource = Names;
+
             if (tbUsername.TextLength < 3 || tbUsername.TextLength > 20)
             {
                 MessageBox.Show("Naam moet tussen 3 - 20 characters zijn.");
@@ -37,7 +39,7 @@ namespace MemoryGameProject
             }
             else
             {
-                lbPlayers.Items.Add(tbUsername.Text);
+                lbPlayers.Items.Add(Names);
                 tbUsername.Clear();
             }
         }
@@ -45,6 +47,26 @@ namespace MemoryGameProject
         private void HowToPlay_Click(object sender, EventArgs e)
         {
             MessageBox.Show("Speel het spel met 2 tot 4 spelers. Ben je met een grotere groep, maak dan teams. ​" + "\n" + "Probeer zoveel mogelijk twee dezelfde kaartjes om te draaien" + "\n" + "Als je aan de beurt bent, draai je een kaartje naar keuze om. Daarna draai je een tweede kaartje om. Als de kaartjes gelijk zijn, dan mag je nog een keer tot je twee kaartjes hebt omgedraaid die géén paar vormen." );
+        }
+
+        private void Play_Click(object sender, EventArgs e)
+        {
+            //TO DO Functie om het spel op te starten.
+        }
+
+        private void Leaderboard_Click(object sender, EventArgs e)
+        {
+            //TO DO Lijst met de beste scores van de spelers.
+        }
+
+        private void About_Click(object sender, EventArgs e)
+        {
+            //TO DO Algemene info over spel etc.
+        }
+
+        private void Exit_Click(object sender, EventArgs e)
+        {
+            this.Close();
         }
     }      
 }
